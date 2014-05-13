5700282a0132ec7dfe21ff666a5181886d7ebc16
Luboš Uhliarik <uhliarik@seznam.cz>
2014-05-13 19:52:46 +0200

    Added trace skript for ftrace

---
+++ scripts/trace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TRACE_PATH="/sys/kernel/debug/tracing"
+
+# nastaveni typu trasovace
+echo "function_graph" > "TRACE_PATH/current_tracer"
+
+# zapsani PID tohoto procesu
+echo "$$" > "$TRACE_PATH/set_ftrace_pid"
+
+# zapnuti trasovani
+echo "1"  > "$TRACE_PATH/tracing_on"
+
+# parametr tohoto skriptu bude 
+# spusten jako trasovany program
+exec "$*"
