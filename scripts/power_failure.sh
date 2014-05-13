e490437090c20aeb07b41630b2c93fd526108050
Luboš Uhliarik <uhliarik@seznam.cz>
2014-05-13 19:48:20 +0200

    Added missing licence to the power_failure.sh file

--- scripts/power_failure.sh
+++ scripts/power_failure.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Copyright (C) 2012-2014 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301, USA.
 
 function setup {
     # FIXME add error checking...
