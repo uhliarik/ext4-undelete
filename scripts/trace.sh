d9e1f8596c79d0995ff2597cd9d475be525d0729
Lubos Uhliarik <uhliarik@seznam.cz>
2014-05-17 22:00:34 +0200

    Fixed bug in trace.sh script

--- scripts/trace.sh
+++ scripts/trace.sh
@@ -2,7 +2,7 @@
 TRACE_PATH="/sys/kernel/debug/tracing"
 
 # nastaveni typu trasovace
-echo "function_graph" > "TRACE_PATH/current_tracer"
+echo "function_graph" > "$TRACE_PATH/current_tracer"
 
 # zapsani PID tohoto procesu
 echo "$$" > "$TRACE_PATH/set_ftrace_pid"
@@ -12,4 +12,4 @@ echo "1"  > "$TRACE_PATH/tracing_on"
 
 # parametr tohoto skriptu bude 
 # spusten jako trasovany program
-exec "$*"
+exec $*
